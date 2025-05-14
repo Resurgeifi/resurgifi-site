dee759d58c60b77431d17a03e8d926f5f41edb9d
Kevin Conzo <kevinconzo@kevins-mbp.mynetworksettings.com>
2025-05-14 19:28:55 -0400

    Final working version with Velessa video, hero box, and new route pages

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-if __name__ == "__main__":
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/sneakpeek')
+def sneakpeek():
+    return render_template('sneakpeek.html')
+@app.route("/testvideo")
+def test_video():
+    return render_template("test_video.html")
+if __name__ == '__main__':
     app.run(debug=True)
+app.run(host='0.0.0.0', port=5001, debug=True)
 
